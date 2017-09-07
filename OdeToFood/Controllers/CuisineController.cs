86a529b9014679c6c947976e2b104b8eca1cf251
Alexander <desarrollador1@azsmart.net>
2017-09-07 10:12:22 -0500

    Primer commit

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        //[Authorize]
+        [Log]
+        public ActionResult Search(string name = "french")
+        {
+            throw new Exception("Error de prueba");
+
+            var message = Server.HtmlEncode(name);
+            //return Content(message);
+            //return RedirectPermanent("http://www.google.com");
+            //return RedirectToAction("Index", "Home", new { name = name } );
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            //return Json(new { Message = message, Name = "Alexander" }, JsonRequestBehavior.AllowGet);
+            //return RedirectToAction("About", "Home", new { name = name });
+            //return RedirectToRoute("AboutDefault");
+            return Content(message);
+        }
+
+        //[HttpGet]
+        //public ActionResult Search()
+        //{
+        //    var message = Server.HtmlEncode("Alexander");
+        //    //return Content(message);
+        //    //return RedirectPermanent("http://www.google.com");
+        //    //return RedirectToAction("Index", "Home", new { name = name } );
+        //    //return File(Server.MapPath("~/Content/site.css"), "text/css");
+        //    //return Json(new { Message = message, Name = "Alexander" }, JsonRequestBehavior.AllowGet);
+        //    //return RedirectToAction("About", "Home", new { name = name });
+        //    //return RedirectToRoute("AboutDefault");
+        //    return Content(message);
+        //}
+    }
+}
---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            ViewBag.MensajeIndex = string.Format("Route: {0} {1} {2}", controller, action, id);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+
+
+            ViewBag.Message = string.Format("Your application description page. {0} {1} {2}", controller, action, id);
+            ViewBag.Location = "Bogota, Colombia.";
+
+            var model = new AboutModel();
+            model.Name = "Jhon Alexander";
+            model.Location = "Bogota, Colombia.";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
